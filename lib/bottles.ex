257d6da9f3683dc4f0046c279995d303a5b70271
Henry Stanley <henry@henrystanley.com>
2015-11-17 20:40:43 +0000

    One stanza of beer song

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -1,6 +1,11 @@
 defmodule Bottles do
   def sing(num) do
-    IO.puts num
+    bottles = to_string(num)
+    IO.puts(bottles <> " bottles of beer on the wall,")
+    IO.puts(bottles <> " bottles of beer.")
+    IO.puts("You take one down, pass it around")
+    num = num - 1
+    IO.puts(to_string(num) <> " bottles of beer on the wall!")
   end
 end
 
