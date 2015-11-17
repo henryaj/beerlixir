3cc6842b289f93ad9ea39a84c955a8cfad7d4de4
Henry Stanley <henry@henrystanley.com>
2015-11-17 23:51:22 +0000

    Now it looks like Ruby

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -1,30 +1,33 @@
 defmodule Bottles do
   def sing(num) when num == 0 do
-    IO.puts("No more bottles of beer on the wall,")
-    IO.puts("no more bottles of beer.")
-    IO.puts("Go to the store and buy some more,")
-    IO.puts("99 bottles of beer on the wall.")
+    IO.puts "No more bottles of beer on the wall,"
+    IO.puts "no more bottles of beer."
+    IO.puts "Go to the store and buy some more,"
+    IO.puts "99 bottles of beer on the wall."
   end
 
   def sing(num) when num == 1 do
-    IO.puts("1 bottle of beer on the wall,")
-    IO.puts("1 bottle of beer.")
-    IO.puts("Take one down and pass it around,")
-    IO.puts("no more bottles of beer on the wall.")
-    IO.puts("")
+    IO.puts "1 bottle of beer on the wall,"
+    IO.puts "1 bottle of beer."
+    IO.puts "Take one down and pass it around,"
+    IO.puts "no more bottles of beer on the wall."
+    IO.puts ""
 
     num = num - 1
+
     Bottles.sing(num)
   end
 
   def sing(num) do
     bottles = to_string(num)
-    IO.puts(bottles <> " bottles of beer on the wall,")
-    IO.puts(bottles <> " bottles of beer.")
-    IO.puts("You take one down, pass it around")
+    IO.puts bottles <> " bottles of beer on the wall,"
+    IO.puts bottles <> " bottles of beer."
+    IO.puts "You take one down, pass it around"
+
     num = num - 1
-    IO.puts(to_string(num) <> " bottles of beer on the wall!")
-    IO.puts("")
+
+    IO.puts to_string(num) <> " bottles of beer on the wall!"
+    IO.puts ""
 
     Bottles.sing(num)
   end
