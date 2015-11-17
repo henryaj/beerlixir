5b4f954ab763c9026c92c3a7cdd5bc1d600cc0b8
Henry Stanley <henry@henrystanley.com>
2015-11-17 23:47:12 +0000

    Proper handling for single bottle scenario :beer:

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -6,13 +6,25 @@ defmodule Bottles do
     IO.puts("99 bottles of beer on the wall.")
   end
 
+  def sing(num) when num == 1 do
+    IO.puts("1 bottle of beer on the wall,")
+    IO.puts("1 bottle of beer.")
+    IO.puts("Take one down and pass it around,")
+    IO.puts("no more bottles of beer on the wall.")
+    IO.puts("")
+
+    num = num - 1
+    Bottles.sing(num)
+  end
+
   def sing(num) do
     bottles = to_string(num)
     IO.puts(bottles <> " bottles of beer on the wall,")
     IO.puts(bottles <> " bottles of beer.")
     IO.puts("You take one down, pass it around")
     num = num - 1
-    IO.puts(to_string(num) <> " bottles of beer on the wall!\n")
+    IO.puts(to_string(num) <> " bottles of beer on the wall!")
+    IO.puts("")
 
     Bottles.sing(num)
   end
