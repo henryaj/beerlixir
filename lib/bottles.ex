253b8e4297308b7727457893470fd0da5bd32816
Henry Stanley <henry@henrystanley.com>
2015-11-17 20:41:29 +0000

    -91307 bottles of beer on the wall

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -6,6 +6,8 @@ defmodule Bottles do
     IO.puts("You take one down, pass it around")
     num = num - 1
     IO.puts(to_string(num) <> " bottles of beer on the wall!")
+
+    Bottles.sing(num)
   end
 end
 
